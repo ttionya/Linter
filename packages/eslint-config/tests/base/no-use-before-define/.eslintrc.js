e70e963fe0584762df702fab54305fad8b0bf1c0
ttionya <git@ttionya.com>
2022-07-23 12:43:31 +0800

    feat(eslint-config): turn off no-use-before-define rule

--- packages/eslint-config/tests/base/no-use-before-define/.eslintrc.js
+++ packages/eslint-config/tests/base/no-use-before-define/.eslintrc.js
@@ -4,16 +4,8 @@ module.exports = {
      * 变量必须先定义后使用
      * @see {@link https://eslint.org/docs/latest/rules/no-use-before-define}
      * @category Base
-     * @reason 存在函数后调用的问题
+     * @reason 存在函数后调用的问题，特别是类中 field 无法直接使用当前类的类型
      */
-    'no-use-before-define': [
-      'error',
-      {
-        variables: false,
-        functions: false,
-        classes: false,
-        allowNamedExports: false,
-      },
-    ],
+    'no-use-before-define': 'off',
   },
 }
