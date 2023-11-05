07bdd4102c1fc28e3bb48e0209de19784d5c465b
ttionya <git@ttionya.com>
2023-11-05 12:47:42 +0800

    chore(prettier-config): add jsdoc plugin rules

--- packages/prettier-config/src/jsdoc.ts
+++ packages/prettier-config/src/jsdoc.ts
@@ -4,7 +4,23 @@ import { defineConfig } from './utils.js'
  * Prettier Plugin jsdoc Options
  *
  * @see https://github.com/hosseinmd/prettier-plugin-jsdoc
+ * @see https://github.com/hosseinmd/prettier-plugin-jsdoc/blob/master/src/index.ts
  */
 export default defineConfig({
   plugins: ['prettier-plugin-jsdoc'],
+
+  jsdocSpaces: 1,
+  jsdocDescriptionWithDot: false,
+  jsdocDescriptionTag: false,
+  jsdocVerticalAlignment: false,
+  jsdocKeepUnParseAbleExampleIndent: true, // false
+  jsdocCommentLineStrategy: 'keep', // singleLine
+  jsdocSeparateReturnsFromParam: false,
+  jsdocSeparateTagGroups: false,
+  jsdocCapitalizeDescription: false, // true
+  tsdoc: false,
+  // jsdocPrintWidth: undefined,
+  jsdocAddDefaultToDescription: false, // true
+  jsdocPreferCodeFences: false,
+  jsdocLineWrappingStyle: 'greedy',
 })
