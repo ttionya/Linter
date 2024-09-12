ae60925a4ac373ef8b9c7905d5e1b7b16cc9425d
ttionya <git@ttionya.com>
2024-09-12 14:01:48 +0800

    chore(lint): remove prettier jsdoc plugin

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,5 +1,3 @@
 import base from '@ttionya/prettier-config'
-import jsdoc from '@ttionya/prettier-config/jsdoc'
-import { mergeConfigs } from '@ttionya/prettier-config/utils'
 
-export default mergeConfigs([base, jsdoc])
+export default base
